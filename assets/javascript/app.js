1f2cfec649e5022848f2bafd611f81c82b476862
SPFlores <sflores92886@gmail.com>
2019-03-22 22:43:20 -0700

    gifs, play/pause, switch working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
-let topics = [`Batman`, `Wonder Woman`, `Captain America`, `Iron Man`, `Spiderman`, `Blue Beetle`, `Black Widow`]
+let topics = [`Batman`, `Wonder Woman`, `Captain America`, `Iron Man`, `Spiderman`, `Blue Beetle`, `Black Widow`, `Superman`]
+let mySwitch = false
 
-// make buttons for each of the names in the array
 const buttonMaker = _ => {
   for (let i = 0; i < topics.length; i++) {
     let gifBtn = document.createElement('button')
@@ -12,31 +12,45 @@ const buttonMaker = _ => {
 }
 
 const makeGifs = data => {
+  document.querySelector('#gifs').innerHTML = ''
+
   data.forEach((gif, i) => {
     let gifImage = document.createElement('img')
-    let paused = gif.images.fixed_height_still.url
-    let playing = gif.images.fixed_height.url
+    let paused = gif.images.fixed_width_still.url
+    let playing = gif.images.fixed_width.url
     let rating = gif.rating
-
     gifImage.src = paused
+    gifImage.className = 'herogif'
     gifImage.setAttribute('data-paused', paused)
     gifImage.setAttribute('data-playing', playing)
     gifImage.setAttribute('data-rating', rating)
-    document.querySelector('#gifs').append(gifImage)
+    gifImage.setAttribute('data-myswitch', mySwitch)
+    document.querySelector('#gifs').appendChild(gifImage)
   })
-
 }
 
-document.addEventListener('click', e => {
-  if (e.target.className === 'gifBtn') {
-    let hero = e.target.dataset.hero
-    let limit = 1
+document.addEventListener('click', ({ target }) => {
+  if (target.className === 'gifBtn') {
+    let hero = target.dataset.hero
+    let limit = 3
     fetch(`http://api.giphy.com/v1/gifs/search?q=${hero}&api_key=jijztlWGO1rAYgw6Q05K9Y3x1XJcXo5T&limit=${limit}`)
       .then(r => r.json())
       .then(r => {
         makeGifs(r.data)
       })
       .catch(e => console.log(e))
+  } else if (target.className === 'herogif') {
+    mySwitch = !mySwitch
+    if (target.dataset.myswitch === 'false') {
+      target.src = target.dataset.playing
+      target.dataset.myswitch = mySwitch
+      console.log(target.dataset.myswitch)
+      console.log('testing')
+    } else {
+      target.src = target.dataset.paused
+      console.log(target.dataset.myswitch)
+      console.log('other testing')
+    }
   }
 })
 
