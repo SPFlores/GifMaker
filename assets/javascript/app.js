348df06e89ead5c26ab80861c68cf9952b4a3453
SPFlores <sflores92886@gmail.com>
2019-03-22 22:50:34 -0700

    input form

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,47 +51,7 @@ document.addEventListener('click', ({ target }) => {
   }
 })
 
-// if you click a gif, it plays
-
 // if you put a name in
 // make button
-
-// fetch(`http://api.giphy.com/v1/gifs/search?q=${animal}&api_key=jijztlWGO1rAYgw6Q05K9Y3x1XJcXo5T&limit=10`)
-//   .then(r => r.json())
-//   .then(({ data }) => {
-//     data.forEach(gif => {
-//       document.querySelector('#gifDiv').innerHTML = ''
-//       const url = gif.images.fixed_height.url
-//       let gifElem = document.createElement('img')
-//       gifElem.setAttribute('src', url)
-//       document.querySelector('#gifDiv').append(gifElem)
-//     })
-//   })
-//   .catch(e => console.log(e))
-
-// let toggle = false
-
-// document.addEventListener('click', ({ target }) => {
-//   if (target.className === 'animal') {
-//     let { animal } = target.dataset
-//     fetch(`https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=${animal}&rating=g`)
-//       .then(r => r.json())
-//       .then(r => {
-//         let { url: still } = r.data.images.fixed_height_still
-//         let { url: animated } = r.data.images.fixed_height
-//         document.querySelector('#gifDiv').innerHTML = `
-//           <img id="gif" src="${still}" alt="${animal}" data-still="${still}" data-animated="${animated}">`
-//       })
-//       .catch(e => console.error(e))
-//   } else if (target.id === 'gif') {
-//     toggle = !toggle
-//     let { still, animated } = target.dataset
-//     if (toggle) {
-//       target.setAttribute('src', animated)
-//     } else {
-//       target.setAttribute('src', still)
-//     }
-//   }
-// })
 
 buttonMaker()
