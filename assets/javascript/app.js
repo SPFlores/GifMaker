a01c6ad9816aa078e1bf9cabc4f79530242c6547
SPFlores <sflores92886@gmail.com>
2019-03-23 20:18:57 -0700

    add 10 more gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,16 @@ const buttonMaker = _ => {
   }
 }
 
-const makeGifs = data => {
+const makeGifs = (data, hero) => {
   document.querySelector('#gifs').innerHTML = ''
-  
+  console.log(hero)
+
+  document.querySelector('#addMoreBtn').innerHTML = ''
   let moreBtn = document.createElement('button')
+  moreBtn.setAttribute('data-hero', hero)
+  moreBtn.id = 'addMore'
   moreBtn.textContent = 'Add 10 more?'
+  document.querySelector('#addMoreBtn').append(moreBtn)
 
   data.forEach((gif, i) => {
     let gifImage = document.createElement('span')
@@ -25,24 +30,23 @@ const makeGifs = data => {
     let playing = gif.images.fixed_width.url
     let rating = gif.rating
     let alt = gif.slug
-    
+
     gifImage.innerHTML = `
     <img src="${paused}" alt="${alt}" class="herogif" data-paused="${paused}" data-playing="${playing}" data-rating="${rating}" data-myswitch="false">
     <h5>Rating: ${rating}</h5>
     `
 
     document.querySelector('#gifs').append(gifImage)
-    document.querySelector('#addMoreBtn').append(moreBtn)
   })
 }
 
 const gifBtnGet = target => {
   let hero = target.dataset.hero
-
   fetch(`http://api.giphy.com/v1/gifs/search?q=${hero}&api_key=jijztlWGO1rAYgw6Q05K9Y3x1XJcXo5T&limit=${limit}`)
     .then(r => r.json())
     .then(r => {
-      makeGifs(r.data)
+      makeGifs(r.data, hero)
+      limit = (r.data).length
     })
     .catch(e => console.log(e))
 }
@@ -59,9 +63,13 @@ const gifPlayer = target => {
 
 document.addEventListener('click', ({ target }) => {
   if (target.className === 'gifBtn') {
+    limit = 10
     gifBtnGet(target)
   } else if (target.className === 'herogif') {
     gifPlayer(target)
+  } else if (target.id === 'addMore') {
+    limit += 10
+    gifBtnGet(target)
   }
 })
 
