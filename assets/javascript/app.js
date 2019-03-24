ded6adb6a380b72e762a4b8ad825bc3db7b11fd3
SPFlores <sflores92886@gmail.com>
2019-03-23 19:31:00 -0700

    add more button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 let topics = [`Batman`, `Wonder Woman`, `Captain America`, `Iron Man`, `Spiderman`, `Blue Beetle`, `Black Widow`, `Superman`]
+let limit = 10
 let hero
 
 const buttonMaker = _ => {
@@ -14,6 +15,9 @@ const buttonMaker = _ => {
 
 const makeGifs = data => {
   document.querySelector('#gifs').innerHTML = ''
+  
+  let moreBtn = document.createElement('button')
+  moreBtn.textContent = 'Add 10 more?'
 
   data.forEach((gif, i) => {
     let gifImage = document.createElement('span')
@@ -21,34 +25,43 @@ const makeGifs = data => {
     let playing = gif.images.fixed_width.url
     let rating = gif.rating
     let alt = gif.slug
-
+    
     gifImage.innerHTML = `
     <img src="${paused}" alt="${alt}" class="herogif" data-paused="${paused}" data-playing="${playing}" data-rating="${rating}" data-myswitch="false">
     <h5>Rating: ${rating}</h5>
     `
 
-    document.querySelector('#gifs').appendChild(gifImage)
+    document.querySelector('#gifs').append(gifImage)
+    document.querySelector('#addMoreBtn').append(moreBtn)
   })
+}
+
+const gifBtnGet = target => {
+  let hero = target.dataset.hero
+
+  fetch(`http://api.giphy.com/v1/gifs/search?q=${hero}&api_key=jijztlWGO1rAYgw6Q05K9Y3x1XJcXo5T&limit=${limit}`)
+    .then(r => r.json())
+    .then(r => {
+      makeGifs(r.data)
+    })
+    .catch(e => console.log(e))
+}
+
+const gifPlayer = target => {
+  if (target.dataset.myswitch === 'false') {
+    target.src = target.dataset.playing
+    target.dataset.myswitch = true
+  } else {
+    target.src = target.dataset.paused
+    target.dataset.myswitch = false
+  }
 }
 
 document.addEventListener('click', ({ target }) => {
   if (target.className === 'gifBtn') {
-    let hero = target.dataset.hero
-    let limit = 10
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${hero}&api_key=jijztlWGO1rAYgw6Q05K9Y3x1XJcXo5T&limit=${limit}`)
-      .then(r => r.json())
-      .then(r => {
-        makeGifs(r.data)
-      })
-      .catch(e => console.log(e))
+    gifBtnGet(target)
   } else if (target.className === 'herogif') {
-    if (target.dataset.myswitch === 'false') {
-      target.src = target.dataset.playing
-      target.dataset.myswitch = true
-    } else {
-      target.src = target.dataset.paused
-      target.dataset.myswitch = false
-    }
+    gifPlayer(target)
   }
 })
 
